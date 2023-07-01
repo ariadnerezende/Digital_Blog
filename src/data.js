b0298f910d011a3fc1083e7ac895e76644ca0f6a
ariad <ariadnederezende@hotmail.com>
2023-07-01 14:27:05 -0300

    criação de elementos HTML e funções

--- src/data.js
+++ src/data.js
@@ -1,24 +1,30 @@
-class Post{
-    constructor(public id: number, public title: string, public imageUrl: string, public body: string){
-
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.body = body;
+var Post = /** @class */ (function () {
+    function Post(id, title, imageUrl, body) {
+        this.id = id;
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.body = body;
+        this.id = id;
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.body = body;
     }
-}
-
- class PostDetails{
-    constructor(public id: number, public postId: number, public email: string, public body: string){
-
-    this.id = id;
-    this.postId = postId;
-    this.email = email;
-    this.body = body;
+    return Post;
+}());
+var PostDetails = /** @class */ (function () {
+    function PostDetails(id, postId, email, body) {
+        this.id = id;
+        this.postId = postId;
+        this.email = email;
+        this.body = body;
+        this.id = id;
+        this.postId = postId;
+        this.email = email;
+        this.body = body;
     }
-}
-
-const posts: Post[] = [
+    return PostDetails;
+}());
+var posts = [
     new Post(1, 'The role of AI in digital transformation', 'https://images.pexels.com/photos/8728379/pexels-photo-8728379.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 'AI plays a transformative role in the digital landscape, revolutionizing industries and driving digital transformation. With its ability to process vast amounts of data and derive actionable insights, AI empowers organizations to make data-driven decisions, enhance operational efficiency, and deliver personalized customer experiences.In the realm of data analysis, AI algorithms can uncover hidden patterns, correlations, and trends that humans might miss. This enables businesses to gain a deeper understanding of their customers, optimize processes, and identify new market opportunities. AI-powered predictive analytics also enables organizations to forecast future outcomes, helping them make proactive decisions and stay ahead of the competition.'),
     new Post(2, 'Data privacy: challenges and solutions in the digital age', 'https://images.pexels.com/photos/5240543/pexels-photo-5240543.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 'Data privacy is a significant concern in the digital age, posing challenges and demanding solutions. With the increasing collection and utilization ofpersonal data, protecting individuals privacy has become paramount. Challenges include ensuring consent, securing data storage, and addressing evolving privacy regulations. Solutions involve implementing robust encryption methods, enhancing user control over data, and promoting transparency in data handling practices. Upholding data privacy rights requires collaboration between organizations, policymakers, and individuals to establish a balance between data-driven innovation and safeguarding personal information.'),
     new Post(3, 'The impact of social media on today society', 'https://images.pexels.com/photos/8088443/pexels-photo-8088443.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 'Social media has had a monumental impact on todays society. It has revolutionized communication, connectivity, and the way information is shared. With billions of users worldwide, social media platforms have become powerful tools for networking, socializing, and even conducting business. However, this pervasive influence has its drawbacks. Privacy concerns, cyberbullying, and the addictive nature of social media have emerged as significant challenges. Moreover, the rapid dissemination of fake news and the amplification of echo chambers have led to polarization and societal divisions. As social media continues to evolve, it is crucial to address these issues and foster a more responsible and inclusive digital environment.'),
@@ -26,8 +32,7 @@ const posts: Post[] = [
     new Post(5, 'Renewable Energy Technologies: towards a sustainable future', 'https://images.pexels.com/photos/9875441/pexels-photo-9875441.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 'Renewable energy technologies are paving the way towards a sustainable future. As the world grapples with the challenges of climate change and depleting fossil fuel resources, the shift towards renewables has become imperative. Solar, wind, hydro, and geothermal power offer clean, abundant, and environmentally friendly alternatives. These technologies have witnessed significant advancements, becoming more efficient and cost-effective over time. By reducing greenhouse gas emissions and dependence on finite resources, renewable energy technologies contribute to mitigating climate change and promoting energy security. As we embrace these sustainable solutions, we move closer to a greener, more resilient future for generations to come.'),
     new Post(6, 'The role of technology in 21st century education', 'https://images.pexels.com/photos/4144224/pexels-photo-4144224.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 'Technology plays a crucial role in 21st-century education, transforming the way we teach and learn. Digital tools and platforms enhance accessibility, collaboration, and engagement in the classroom. Interactive whiteboards, online resources, and educational apps provide dynamic and personalized learning experiences. Technology also enables distance learning, breaking down geographical barriers and expanding educational opportunities. Moreover, it equips students with essential digital literacy skills for the modern workforce. However, effective integration and proper digital citizenship education are vital to maximize the benefits of technology in education and ensure equitable access for all learners.'),
 ];
-
-const commentsPosts: PostDetails[] = [
+var commentsPosts = [
     new PostDetails(1, 1, 'sarah.williams@example.com: ', 'Artificial Intelligence has truly revolutionized the way businesses operate in the digital age. Its ability to analyze vast amounts of data and make informed decisions in real-time has enabled companies to streamline their processes and improve efficiency.'),
     new PostDetails(1, 1, 'alexander.brown@example.com: ', 'Im amazed at the impact of Artificial Intelligence on digital transformation. From predictive analytics to machine learning algorithms, AI has opened up new possibilities for businesses to gain insights and make data-driven decisions.'),
     new PostDetails(1, 1, 'jacob.harris@example.com: ', 'The ability of AI to analyze data and make informed decisions is revolutionizing industries.'),
@@ -47,119 +52,95 @@ const commentsPosts: PostDetails[] = [
     new PostDetails(6, 6, 'olivia.davis@example.com: ', 'In the 21st century, technology has become an integral part of education, opening up new possibilities for teaching and learning. Digital devices, educational apps, and online platforms have made education more accessible, flexible, and engaging. Technology facilitates distance learning, collaborative projects, and personalized instruction, catering to diverse learning styles and needs.'),
     new PostDetails(6, 6, 'oliver.green@example.com: ', 'Incorporating technology in education prepares students for the digital demands of the modern world.'),
 ];
-
-document.addEventListener('DOMContentLoaded', () => {
-    const postsContainer = document.getElementById('postsContainer');
-  
-    if (!postsContainer) return;
-  
-    posts.forEach((post) => {
-      const postElement = document.createElement('div');
-      postElement.id = 'div1';
-  
-      postElement.addEventListener('click', () => {
-        setDetails(post.id);
-      });
-  
-      postElement.addEventListener('mouseover', () => {
-        postElement.style.cursor = 'pointer';
-      });
-  
-      postElement.addEventListener('mouseout', () => {
-        postElement.style.cursor = 'default';
-      });
-  
-      const imageElement = document.createElement('img');
-      imageElement.src = post.imageUrl;
-      postElement.appendChild(imageElement);
-      imageElement.classList.add('image-post');
-  
-      const titleElement = document.createElement('h3');
-      titleElement.textContent = post.title;
-      postElement.appendChild(titleElement);
-      titleElement.classList.add('title-post');
-  
-      const bodyElement = document.createElement('p');
-      bodyElement.textContent = post.body;
-      postElement.appendChild(bodyElement);
-      bodyElement.classList.add('body-post');
-  
-      const goExpand = document.createElement('p');
-      goExpand.textContent = 'Expand...';
-      bodyElement.appendChild(goExpand);
-      goExpand.classList.add('expand-post');
-  
-      postsContainer.appendChild(postElement);
+document.addEventListener('DOMContentLoaded', function () {
+    var postsContainer = document.getElementById('postsContainer');
+    if (!postsContainer)
+        return;
+    posts.forEach(function (post) {
+        var postElement = document.createElement('div');
+        postElement.id = 'div1';
+        postElement.addEventListener('click', function () {
+            setDetails(post.id);
+        });
+        postElement.addEventListener('mouseover', function () {
+            postElement.style.cursor = 'pointer';
+        });
+        postElement.addEventListener('mouseout', function () {
+            postElement.style.cursor = 'default';
+        });
+        var imageElement = document.createElement('img');
+        imageElement.src = post.imageUrl;
+        postElement.appendChild(imageElement);
+        imageElement.classList.add('image-post');
+        var titleElement = document.createElement('h3');
+        titleElement.textContent = post.title;
+        postElement.appendChild(titleElement);
+        titleElement.classList.add('title-post');
+        var bodyElement = document.createElement('p');
+        bodyElement.textContent = post.body;
+        postElement.appendChild(bodyElement);
+        bodyElement.classList.add('body-post');
+        var goExpand = document.createElement('p');
+        goExpand.textContent = 'Expand...';
+        bodyElement.appendChild(goExpand);
+        goExpand.classList.add('expand-post');
+        postsContainer.appendChild(postElement);
     });
-  
-    function getPostDetailsId(postId: number): PostDetails[] {
-      return commentsPosts.filter((comment) => comment.postId === postId);
+    function getPostDetailsId(postId) {
+        return commentsPosts.filter(function (comment) { return comment.postId === postId; });
     }
-  
-    function getPostId(id: number): Post | undefined {
-      for (let i = 0; i < posts.length; i++) {
-        if (posts[i].id === id) {
-          return posts[i];
+    function getPostId(id) {
+        for (var i = 0; i < posts.length; i++) {
+            if (posts[i].id === id) {
+                return posts[i];
+            }
         }
-      }
-      return undefined;
+        return undefined;
     }
-  
-    function setDetails(postId: number) {
-      const postsContainer = document.getElementById('postsContainer');
-      if (!postsContainer) return;
-    
-      const post = getPostId(postId);
-      if (!post) return;
-    
-      postsContainer.innerHTML = '';
-    
-      const postDetailsContainer = document.createElement('div');
-      postsContainer.appendChild(postDetailsContainer);
-      postDetailsContainer.id = 'div2';
-    
-      const imageElement = document.createElement('img');
-      imageElement.src = post.imageUrl;
-      postDetailsContainer.appendChild(imageElement);
-      imageElement.classList.add('image-comments');
-    
-      const titleElement = document.createElement('h3');
-      titleElement.textContent = post.title;
-      postDetailsContainer.appendChild(titleElement);
-      titleElement.classList.add('title-comments');
-    
-      const bodyElement = document.createElement('p');
-      bodyElement.textContent = post.body;
-      postDetailsContainer.appendChild(bodyElement);
-      bodyElement.classList.add('body-comments');
-    
-      const commentsElement = document.createElement('div');
-      postDetailsContainer.appendChild(commentsElement);
-      commentsElement.id = 'div3';
-    
-      const postDetails = getPostDetailsId(post.id);
-      
-      postDetails.forEach((comment, index) => {
-        const commentElement = document.createElement('p');
-  
-        const emailSpanElement = document.createElement('span');
-        emailSpanElement.textContent = `user_ ${index + 1}: ${comment.email}`;
-        emailSpanElement.style.fontWeight = 'bold';
-        emailSpanElement.classList.add('email-comments');
-        commentElement.appendChild(emailSpanElement);
-    
-        const commentBodyElement = document.createElement('span');
-        commentBodyElement.textContent = comment.body;
-        commentBodyElement.classList.add('body-comments');
-        commentElement.appendChild(commentBodyElement);
-  
-        commentsElement.appendChild(commentElement);
-      });
-    
-      const backButton = document.createElement('a');
-      backButton.classList.add('back-button');
-      backButton.href = 'index.html';
-      backButton.textContent = 'Go Back';
-      postDetailsContainer.appendChild(backButton);
+    function setDetails(postId) {
+        var postsContainer = document.getElementById('postsContainer');
+        if (!postsContainer)
+            return;
+        var post = getPostId(postId);
+        if (!post)
+            return;
+        postsContainer.innerHTML = '';
+        var postDetailsContainer = document.createElement('div');
+        postsContainer.appendChild(postDetailsContainer);
+        postDetailsContainer.id = 'div2';
+        var imageElement = document.createElement('img');
+        imageElement.src = post.imageUrl;
+        postDetailsContainer.appendChild(imageElement);
+        imageElement.classList.add('image-comments');
+        var titleElement = document.createElement('h3');
+        titleElement.textContent = post.title;
+        postDetailsContainer.appendChild(titleElement);
+        titleElement.classList.add('title-comments');
+        var bodyElement = document.createElement('p');
+        bodyElement.textContent = post.body;
+        postDetailsContainer.appendChild(bodyElement);
+        bodyElement.classList.add('body-comments');
+        var commentsElement = document.createElement('div');
+        postDetailsContainer.appendChild(commentsElement);
+        commentsElement.id = 'div3';
+        var postDetails = getPostDetailsId(post.id);
+        postDetails.forEach(function (comment, index) {
+            var commentElement = document.createElement('p');
+            var emailSpanElement = document.createElement('span');
+            emailSpanElement.textContent = "user_ ".concat(index + 1, ": ").concat(comment.email);
+            emailSpanElement.style.fontWeight = 'bold';
+            emailSpanElement.classList.add('email-comments');
+            commentElement.appendChild(emailSpanElement);
+            var commentBodyElement = document.createElement('span');
+            commentBodyElement.textContent = comment.body;
+            commentBodyElement.classList.add('body-comments');
+            commentElement.appendChild(commentBodyElement);
+            commentsElement.appendChild(commentElement);
+        });
+        var backButton = document.createElement('a');
+        backButton.classList.add('back-button');
+        backButton.href = 'index.html';
+        backButton.textContent = 'Go Back';
+        postDetailsContainer.appendChild(backButton);
     }
-  });
+});
